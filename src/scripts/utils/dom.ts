44c4df5b8495a194a5e8cdb256bab54d83b7307a
Tuan Quoc Phan <tuan.phan@asnet.com.vn>
2023-09-13 16:54:58 +0700

    fix(function-type): refactor function type

--- src/scripts/utils/dom.ts
+++ src/scripts/utils/dom.ts
@@ -3,27 +3,23 @@
  * @param {string} selector - The selector for the HTML element.
  * @returns {T } The queried HTML element or null if not found.
  */
-function querySelector<T extends HTMLElement>(selector: string): T {
-  return document.querySelector(selector) as T;
-}
+const querySelector = <T extends HTMLElement>(selector: string): T =>
+  document.querySelector(selector) as T;
 
 /**
  * Queries all HTML elements based on the given selector.
  * @param {string} selector - The selector for the HTML elements.
  * @returns {NodeListOf<T>} A list of queried HTML elements.
  */
-function querySelectorAll<T extends HTMLElement>(selector: string): NodeListOf<T> {
-  return document.querySelectorAll(selector);
-}
+const querySelectorAll = <T extends HTMLElement>(selector: string): NodeListOf<T> =>
+  document.querySelectorAll(selector);
 
 /**
  * Retrieves an HTML element by its id.
  * @param {string} id - The id of the HTML element.
  * @returns {T } The HTML element with the specified id or null if not found.
  */
-function getElementById<T extends HTMLElement>(id: string): T {
-  return document.getElementById(id) as T;
-}
+const getElementById = <T extends HTMLElement>(id: string): T => document.getElementById(id) as T;
 
 /**
  * Create an HTML element with the specified tag name and attributes.
@@ -32,10 +28,10 @@ function getElementById<T extends HTMLElement>(id: string): T {
  * @param {Record<string, string>} attributes - An object containing key-value pairs representing attributes to add to the element.
  * @returns {HTMLElement} - The created HTML element.
  */
-function createElement<K extends keyof HTMLElementTagNameMap>(
+const createElement = <K extends keyof HTMLElementTagNameMap>(
   tagName: K,
   attributes?: Record<string, string>,
-): HTMLElementTagNameMap[K] {
+): HTMLElementTagNameMap[K] => {
   const element = document.createElement(tagName);
 
   if (attributes) {
@@ -45,6 +41,6 @@ function createElement<K extends keyof HTMLElementTagNameMap>(
   }
 
   return element;
-}
+};
 
 export { querySelector, querySelectorAll, getElementById, createElement };
